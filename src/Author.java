953e44db19ea0f0a482c5b5d256d7521c6313890
vzavolsky <vzavolsky@ya.ru>
2023-03-03 01:11:49 +0300

    Homework 12 - Object and Classes. Task code.

---
+++ src/Author.java
@@ -0,0 +1,16 @@
+public class Author {
+    private String authorName, authorFamilyName;
+
+    public Author(String authorName, String authorFamilyName) {
+        this.authorName = authorName;
+        this.authorFamilyName = authorFamilyName;
+    }
+
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    public String getAuthorFamilyName() {
+        return this.authorFamilyName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    String bookName;
+    Author author;
+    int publicYear;
+
+    public Book(String bookName, Author author, int publicYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publicYear = publicYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getPublicYear() {
+        return this.publicYear;
+    }
+
+    public void setPublicYear(int publicYear) {
+        this.publicYear = publicYear;
+    }
+}
